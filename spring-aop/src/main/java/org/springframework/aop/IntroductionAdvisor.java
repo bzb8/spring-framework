3127bdc9522702f8bfac4c3e1eff6d32490fb4c6
baizhibin <baizhibin@kedacom.com>
2024-02-27 09:56:27 +0800

    源码注释添加

--- spring-aop/src/main/java/org/springframework/aop/IntroductionAdvisor.java
+++ spring-aop/src/main/java/org/springframework/aop/IntroductionAdvisor.java
@@ -27,8 +27,6 @@ package org.springframework.aop;
  * --
  * 一个Java类，没有实现A接口，在不修改Java类的情况下，使其具备A接口的功能。可以通过IntroductionAdvisor给目标类引入更多接口的功能，这个功能是不是非常牛逼。
  *
- * 一个Java类，没有实现A接口，在不修改Java类的情况下，使其具备A接口的功能。可以通过IntroductionAdvisor给目标类引入更多接口的功能，这个功能是不是非常牛逼。
- *
  * @author Rod Johnson
  * @since 04.04.2003
  * @see IntroductionInterceptor
